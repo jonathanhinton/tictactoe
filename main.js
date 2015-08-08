be0886d85a13ea1361238cbc9f5100d59012b438
Jonathan Hinton <jonathanhinton@Jonathans-MacBook-Pro.local>
2015-08-08 13:51:26 -0500

    progress on program

--- main.js
+++ main.js
@@ -1,10 +1,130 @@
-var player1 = prompt("Please type your name player 1");
-var player2 = prompt("Please type your name player 2");
-console.log(player1);
-console.log(player2);
 
-var player1name = document.getElementById("playerone");
-var player2name = document.getElementById("playertwo");
+//THIS IS MY CODE THAT DOESN'T WORK
+//
+var board = [a1, a2, a3, b1, b2, b3, c1, c2, c3]
+console.log(board);
 
-player1name.innerHTML = player1;
-player2name.innerHTML = player2;
+
+var wins = {
+  win1: [a1, a2, a3],
+  win2: [b1, b2, b3],
+  win3: [c1, c2, c3],
+  win4: [a1, b1, c1],
+  win5: [a2, b2, c2],
+  win6: [a3, b3, c3],
+  win7: [a1, b2, c3],
+  win8: [a3, b2, c1]
+}
+console.log(wins);
+
+var turn = "x";
+var score = "";
+var x = [];
+var o = [];
+
+if (turn === "x") {
+  turn = "o";
+  } else {
+  turn = "x";
+}
+
+// THIS IS SOMEONE ELSE'S CODE THAT WORKS
+(function () { //this starts the whole thing
+
+    var squares = [], //empty array
+        EMPTY = "\xA0",
+        score,
+        moves,
+        turn = "X",
+        oldOnload,
+        wins = [7, 56, 448, 73, 146, 292, 273, 84],
+        startNewGame = function () {
+            var i;
+
+            turn = "X";
+            score = {"X": 0, "O": 0};
+            moves = 0;
+            for (i = 0; i < squares.length; i += 1) {
+                squares[i].firstChild.nodeValue = EMPTY;
+            }
+        },
+        win = function (score) {
+            var i;
+            for (i = 0; i < wins.length; i += 1) {
+                if ((wins[i] & score) === wins[i]) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        set = function () {
+            if (this.firstChild.nodeValue !== EMPTY) {
+                return;
+            }
+            this.firstChild.nodeValue = turn;
+            moves += 1;
+            score[turn] += this.indicator;
+            if (win(score[turn])) {
+                alert(turn + " wins!");
+                startNewGame();
+            } else if (moves === 9) {
+                alert("Cat\u2019s game!");
+                startNewGame();
+            } else {
+                turn = turn === "X" ? "O" : "X";
+            }
+        },
+        play = function () {
+            var board = document.createElement("table"),
+                indicator = 1,
+                i, j,
+                row, cell,
+                parent;
+            board.border = 1;
+            for (i = 0; i < 3; i += 1) {
+                row = document.createElement("tr");
+                board.appendChild(row);
+                for (j = 0; j < 3; j += 1) {
+                    cell = document.createElement("td");
+                    cell.width = cell.height = 50;
+                    cell.align = cell.valign = 'center';
+                    cell.indicator = indicator;
+                    cell.onclick = set;
+                    cell.appendChild(document.createTextNode(""));
+                    row.appendChild(cell);
+                    squares.push(cell);
+                    indicator += indicator;
+                }
+            }
+        parent = document.getElementById("tictactoe") || document.body;
+        parent.appendChild(board);
+        startNewGame();
+    };
+    if (typeof window.onload === "function") {
+        oldOnLoad = window.onload;
+        window.onload = function () {
+            oldOnLoad();
+            play();
+        };
+    } else {
+        window.onload = play;
+    }
+}());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
